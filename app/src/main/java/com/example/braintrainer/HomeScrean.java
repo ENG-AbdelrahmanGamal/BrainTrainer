27d31b149fe0a5766b2e96cba819a70947d4bb85
Abdelrahman Gamal <abdelrahmanbetter@stud.cu.edu.eg>
2021-07-20 21:02:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/braintrainer/HomeScrean.java
@@ -0,0 +1,145 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class HomeScrean extends AppCompatActivity {
+Button button0,button1,button2,button3,tryAgainButton;
+TextView timerTextView,scoreTextView,functionTextView ,resultTextView;
+    private static final String TAG = "HomeScrean";
+    int correctLocation;
+    int score=0;
+int numberOfQuestion=0;
+
+@SuppressLint("SetTextI18n")
+public void newQuestion()
+{
+    sound();
+    tryAgainButton.setVisibility(View.INVISIBLE);
+    resultTextView.setVisibility(View.VISIBLE);
+    ArrayList<Integer>answer=new ArrayList<>();
+    Random random=new Random();
+    int a =random.nextInt(21);
+    int b=random.nextInt(21);
+    functionTextView.setText(a+"+"+b);
+    correctLocation=random.nextInt(4);
+    answer.clear();
+    for (int i = 0; i < 4; i++)
+    {
+        if(i==correctLocation)
+        {  answer.add(a+b);}
+
+        else {
+            int wrongAnswer=random.nextInt(41);
+            while (wrongAnswer==a+b)
+            {
+                wrongAnswer=random.nextInt(41);
+            }
+            answer.add(wrongAnswer);
+
+        }
+
+//            button0.setText(answer.get(0));
+//            button1.setText(answer.get(1));
+//            button2.setText(answer.get(2));
+//            button3.setText(answer.get(3));
+
+
+
+    }
+    button0.setText(Integer.toString(answer.get(0)));
+    button1.setText(Integer.toString(answer.get(1)));
+    button2.setText(Integer.toString(answer.get(2)));
+    button3.setText(Integer.toString(answer.get(3)));
+}
+
+
+    @SuppressLint("SetTextI18n")
+    public void chooseAnswer(View view) {
+
+        if( Integer.toString(correctLocation).equals(view.getTag().toString()))
+        {
+            resultTextView.setText("Correct");
+            score++;
+            Log.i(TAG, "chooseAnswer:  Correct");}
+        else
+        {
+            resultTextView.setText("Wrong Answer");
+            Log.i(TAG, "chooseAnswer:  Wrong Answer");
+        }
+        numberOfQuestion++;
+        scoreTextView.setText(score+"/"+numberOfQuestion);
+        newQuestion();
+}
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screan);
+        functionTextView=findViewById(R.id.textView_function);
+        scoreTextView=findViewById(R.id.textView_score);
+        button0=findViewById(R.id.button0);
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        resultTextView=findViewById(R.id.result_textview);
+        timerTextView=findViewById(R.id.textView_Timer);
+        tryAgainButton=findViewById(R.id.againPlay_button);
+        newQuestion();
+        timer(30100,1000);
+
+    }
+
+
+    public void tryAgain(View view) {
+    score=0;
+    numberOfQuestion=0;
+    scoreTextView.setText(score+"/"+numberOfQuestion);
+        resultTextView.setVisibility(View.INVISIBLE);
+        tryAgainButton.setVisibility(View.INVISIBLE);
+        timer(30100,1000);
+        newQuestion();
+
+    }
+
+    private void timer(int i, int i1) {
+        new CountDownTimer(i, i1) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText( String.valueOf(millisUntilFinished/1000+"S"));
+
+            }
+
+
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Hey Time Finish ");
+                tryAgainButton.setVisibility(View.VISIBLE);
+
+
+            }
+        }.start();
+
+
+    }
+    public void sound()
+    {
+        MediaPlayer mediaPlayer= MediaPlayer.create(getApplicationContext(),R.raw.counter);
+        mediaPlayer.start();
+
+    }
+}
---
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        Intent intent = new Intent(this,HomeScrean.class);
+        startActivity(intent);
+       // button.setVisibility(view.VISIBLE);
+
+    }
+
+}
